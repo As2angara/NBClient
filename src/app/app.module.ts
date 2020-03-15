51b389ef337f3cbd757e932fd86df64c064dc986
alasalio <obadaasali1@gmail.com>
2020-03-15 17:39:04 -0400

    created part of landing page however section separator currently broken

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingModule } from './modules/landing/landing.module';
+import { NavigationbarComponent } from './components/navigationbar/navigationbar.component';
+// import { SectionSeparatorComponent } from './components/section-separator/section-separator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LandingModule
   ],
   providers: [],
+  exports: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-
-
+import { AboutNikoComponent } from './components/about-niko/about-niko.component';
+import {MenuComponent} from './components/menu/menu.component';
+import {SectionSeparatorComponent} from '../../components/section-separator/section-separator.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, AboutNikoComponent, MenuComponent, SectionSeparatorComponent],
+  exports: [
+    HeaderComponent,
+    AboutNikoComponent,
+    MenuComponent,
+    SectionSeparatorComponent
+  ],
   imports: [
     CommonModule
   ]
