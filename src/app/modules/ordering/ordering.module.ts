78fdd4704a89e5ad9d39505495298be25bd07d9f
AdrianSA <adrian.angara@gmail.com>
2020-03-15 13:08:54 -0400

    Added json files to simulate backend calls to database

--- src/app/modules/ordering/ordering.module.ts
+++ src/app/modules/ordering/ordering.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuCategoryComponent } from './components/menu-category/menu-category.component';
+import { OrderDashboardComponent } from './components/order-dashboard/order-dashboard.component';
+import {MenuComponent} from './components/menu/menu.component';
+import {MenuItemComponent} from './components/menu-item/menu-item.component';
 
 
 
 @NgModule({
-  declarations: [MenuComponent, MenuItemComponent],
+  declarations: [MenuComponent, MenuItemComponent, MenuCategoryComponent, OrderDashboardComponent],
   imports: [
     CommonModule
   ]
 })
+
 export class OrderingModule { }
