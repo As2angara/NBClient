620bfec44e4d28366d8cf9fcc82b4f19ad1ea70a
AdrianSA <Adrian.angara@gmail.com>
2020-03-14 23:03:08 -0400

    Added the menu-item componenet into ordering module

--- src/app/modules/ordering/ordering.module.ts
+++ src/app/modules/ordering/ordering.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './components/menu/menu.component';
+import { MenuItemComponent } from './components/menu-item/menu-item.component';
+
 
 
 
 @NgModule({
-  declarations: [MenuComponent],
+  declarations: [MenuComponent, MenuItemComponent],
   imports: [
     CommonModule
   ]
