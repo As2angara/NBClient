c84fe4bda4d5645b3d817b2a76c3ed8d60e3c78f
AdrianSA <adrian.angara@gmail.com>
2020-04-16 19:49:44 -0400

    Implemented ngFor and ngIf statements to reduce repeated code on landing page.

--- src/app/modules/landing/components/menu/menu.component.ts
+++ src/app/modules/landing/components/menu/menu.component.ts
@@ -2,6 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import {OrderDashboardComponent} from '../../../ordering/components/order-dashboard/order-dashboard.component';
 import {MatDialog} from '@angular/material/dialog';
 import {MenuCategory} from '../../../../models/menu-category';
+import {MenuItem} from '../../../../models/menu-item';
+import {OnlineMenuService} from '../../../ordering/services/online-menu.service';
+import {Observable} from 'rxjs';
+
+const cat = [
+  {
+    id: 2,
+    name: 'Entrees'
+  },
+  {
+    id: 3,
+    name: 'Sides'
+  },
+  {
+    id: 4,
+    name: 'Desserts'
+  }
+];
+
 
 @Component({
   selector: 'app-menu-landing',
@@ -11,24 +30,14 @@ import {MenuCategory} from '../../../../models/menu-category';
 export class MenuComponent implements OnInit {
 
   categories: MenuCategory[];
+  items$: Observable<MenuItem[]>;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog,
+              private menu: OnlineMenuService) { }
 
   ngOnInit() {
-    this.categories = [
-      {
-        id: 2,
-        name: 'Entrees'
-      },
-      {
-        id: 3,
-        name: 'Sides'
-      },
-      {
-        id: 4,
-        name: 'Desserts'
-      }
-    ];
+    this.categories = cat;
+    this.items$ = this.menu.getMenuItems();
   }
 
  openOrderDashboard() {
